2c504005b6e454d9bb06ccbc36c5d585d1a7d109
BinyamF <binyamfitamo7@gmail.com>
2020-12-17 10:37:14 +0300

    added file upload request handler on index.js  file

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
+var cors = require('cors');
+app.use(cors());
+app.use(express.static('public'));
+app.use(fileUpload());
 require('./routes/index')(app);
 
+
+app.post('/upload', (req, res) => {
+  if (!req.files) {
+      return res.status(500).send({ msg: "file is not found" })
+  }
+  const myFile = req.files.file;
+  myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+      if (err) {
+          console.log(err)
+          return res.status(500).send({ msg: "Error occured" });
+      }
+      return res.send({name: myFile.name, path: `/${myFile.name}`});
+  });
+})
 
 if (!keys.jwtPrivateKey) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
