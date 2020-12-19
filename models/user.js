e9fdc8581cef7af3849e308f5d56c43534a5e638
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2020-12-19 10:24:06 +0300

    Added create, search, edit and delete user functionlity

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-const subscriberRequestSchema=require('../models/subscriber_request');
+const subscriberRequestSchema = require('../models/subscriber_request');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -19,23 +19,37 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 1024
   },
-  isAdmin: Boolean,
-  isActive: Boolean,
-  loginLink:{
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+
+  isActive: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+
+  // isAdmin: Boolean,
+  // isActive: Boolean,
+
+  loginLink: {
     type: String,
     required: false,
     unique: true
   },
-  subscriberRequest:{
-    type:subscriberRequestSchema
+  subscriberRequest: {
+    type: subscriberRequestSchema
   },
-  createdAt:{ 
-    type : Date, 
-    default: Date.now 
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
-userSchema.methods.generateAuthToken = function() { 
+userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, keys.jwtPrivateKey);
   return token;
 }
@@ -45,12 +59,14 @@ const User = mongoose.model('User', userSchema);
 function validateUser(user) {
   const schema = Joi.object({
     username: Joi.string().min(5).max(255).required(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(5).max(255).required(),
+    isAdmin: Joi.boolean(),
+    isActive: Joi.boolean(),
   });
   const validation = schema.validate(user);
   return validation;
 }
 
 
-exports.User = User; 
-exports.validateUser= validateUser;
+exports.User = User;
+exports.validateUser = validateUser;
