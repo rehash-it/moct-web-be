e19c8c3fd0436fe2b43d9069af377b96d35815f2
BinyamF <binyamfitamo7@gmail.com>
2020-12-28 17:51:27 +0300

    backend api post endpoint and image folder created

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,4 @@ module.exports = {
   jwtPrivateKey : process.env.JWT_SECRET||'dsfsdfkasdjfkasdhfkashdflashfdfsdaf',
   dburl:process.env.DATABASE_URL||'mongodb+srv://root:25524825233@cluster0.mlhns.mongodb.net/tourgallery?retryWrites=true&w=majority',
   port:process.env.PORT || 4500,
-  appurl:"localhost:4500"
 }
