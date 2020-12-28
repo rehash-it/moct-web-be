e5468530411739f8840734de060b51f3c7a030ef
BinyamF <binyamfitamo7@gmail.com>
2020-12-28 17:56:25 +0300

    port number corrected

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   jwtPrivateKey : process.env.JWT_SECRET||'dsfsdfkasdjfkasdhfkashdflashfdfsdaf',
   dburl:process.env.DATABASE_URL||'mongodb+srv://root:25524825233@cluster0.mlhns.mongodb.net/tourgallery?retryWrites=true&w=majority',
-  port:process.env.PORT || 4500,
+  port:process.env.PORT || 6000
 }
