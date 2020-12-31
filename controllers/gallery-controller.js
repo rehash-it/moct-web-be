fe5529b4e5fe786db349a564aa726b4ec5fd3ece
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2020-12-31 14:40:19 +0300

    solved get gallery by category issuue

--- controllers/gallery-controller.js
+++ controllers/gallery-controller.js
@@ -108,7 +108,7 @@ exports.deleteGallery =async (req, res) => {
 };
 
 exports.getGalleryByCategory = async (req, res) => {
-    const gallery= await Gallery.find(req.params.id).select({ });
+    const gallery= await Gallery.find(req.params.category).select({ });
   
     if (!gallery) return res.status(404).send('The Gallery with the given ID was not found.');
   
