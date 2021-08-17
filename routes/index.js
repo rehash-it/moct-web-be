0e7c2f722dbbbef3d65cbedb344f4ba328caf78b
Rehana Abdulber <rabdulber@excellerentsolutions.com>
2021-08-18 01:25:38 +0300

    rehana's fix

--- routes/index.js
+++ routes/index.js
@@ -117,7 +117,7 @@ module.exports = function (app) {
     "/api",
     router.post(
       "/user",
-      [auth, admin],
+      // [auth, admin],
       asyncMiddleware(userController.createUser)
     )
   );
